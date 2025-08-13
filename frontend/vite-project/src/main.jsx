7041ee661efc9437f685e27cd59fd92fccfecc8b
Diya Betcy <diyabetcy123@gmail.com>
2025-08-14 00:06:14 +0530

    add folders and set basic ui and routes

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Import pages
+import Base from './pages/base.jsx';
+import Login from "./pages/login.jsx";
+import Dashboard from "./pages/dashboard.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Base />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ frontend/vite-project/src/pages/base.jsx
@@ -0,0 +1,15 @@
+import "./base.css";
+
+export default function Base() {
+  return (
+    <div className="base-container">
+      <div className="top-bar">
+        <img src="/src/assets/logo.png" alt="Logo" className="logo-image" />
+      </div>
+
+      <div className="main-content">
+        
+      </div>
+    </div>
+  );
+}
