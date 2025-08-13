3ac102e0a5c258119d8f53e386c3c1e78ec50d12
Diya Betcy <diyabetcy123@gmail.com>
2025-08-14 00:35:30 +0530

    signup page setup ready

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Import pages
-import Base from './pages/base.jsx';
+import Signup from "./pages/signup.jsx";
 import Login from "./pages/login.jsx";
 import Dashboard from "./pages/dashboard.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Base />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
 );
--- frontend/vite-project/src/pages/signup.jsx
+++ frontend/vite-project/src/pages/signup.jsx
@@ -16,12 +16,21 @@ export default function Signup() {
         <div className="right-panel">
           <div className="form-wrapper">
             <h2>Create Account</h2>
+             <button className="google-btn">Sign up with Google</button>
+             <p className="or-text">— or —</p>
             <form>
               <input type="text" placeholder="Full Name" />
               <input type="email" placeholder="Email" />
+              <p className="send-otp">
+              <a href="/send-otp">Send OTP</a>
+              </p>
               <input type="password" placeholder="Password" />
-              <button type="submit">Sign Up</button>
+              <input type="password" placeholder="Confirm Password" />
+              <button className="submit" type="submit">Sign Up</button>
             </form>
+            <p className="already-account">
+            Already have an account? <a href="/login">Login</a>
+            </p>
           </div>
         </div>
       </div>
