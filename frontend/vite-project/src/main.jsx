2759cceee8a7781aacc9da6f7080f5a7c21911a2
RAIHANA K.M <raihanakmb@gmail.com>
2025-08-19 21:58:11 +0530

    add start.css and start.jsx to  main

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/signup.jsx";
 import Login from "./pages/login.jsx";
 import Dashboard from "./pages/dashboard.jsx";
-
+import Start from "./pages/start.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path ="/start"  element ={<Start />} />
     </Routes>
   </BrowserRouter>
 );
